017e3280414bc8736775f8bc82e2c732a6d2cf1e
ybaha <58085088+ybaha@users.noreply.github.com>
2021-03-28 20:53:39 +0300

    procfile

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
 
 const fs = require('fs')
 const fsP = require('fs').promises
